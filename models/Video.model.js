6c7c2aee9b2f3811756c8e3715e1725be22e8047
jitu <jitukumar63766@gmail.com>
2025-07-20 01:21:18 +0530

    feat: enhance video interaction features; add watch functionality, improve like/dislike logic, and update Video model schema

--- models/Video.model.js
+++ models/Video.model.js
@@ -1,43 +1,53 @@
 import mongoose from "mongoose";
 
-const videoSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
+const videoSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    url: {
+      type: String,
+      required: true,
+    },
+    thumbnail: {
+      type: String,
+      required: true,
+    },
+    uploader: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    views: {
+      type: Number,
+      default: 0,
+    },
+    viewedBy: {
+      type: [mongoose.Schema.Types.ObjectId],
+      ref: "User",
+      default: [],
+    },
+    likes: {
+      type: [mongoose.Schema.Types.ObjectId],
+      ref: "User",
+      default: [],
+    },
+    dislikes: {
+      type: [mongoose.Schema.Types.ObjectId],
+      ref: "User",
+      default: [],
+    },
+    comments: {
+      type: Array,
+      default: [],
+    },
   },
-  description: {
-    type: String,
-  },
-  url: {
-    type: String,
-    required: true,
-  },
-  thumbnail: {
-    type: String,
-    required: true,
-  },
-  uploader: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
-  views: {
-    type: Number,
-    default: 0,
-  },
-  likes: {
-    type: Number,
-    default: 0,
-  },
-  dislikes: {
-    type: Number,
-    default: 0,
-  },
-  comments: {
-    type: Array,
-    default: [],
-  },
-}, { timestamps: true });
+  { timestamps: true }
+);
 
 const Video = mongoose.model("Video", videoSchema);
 
